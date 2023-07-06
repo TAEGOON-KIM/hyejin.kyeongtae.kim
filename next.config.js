09e8fc37908437496289bcc16c10491176c98d90
TAEGOON-KIM <138659521+TAEGOON-KIM@users.noreply.github.com>
2023-07-06 11:37:17 +0900

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
+    // output: 'export',
     images: {
         unoptimized: true, // 이미지 최적화 기능을 비활성화합니다.
     }
