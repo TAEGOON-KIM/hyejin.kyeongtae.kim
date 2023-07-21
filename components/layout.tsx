5b5c252d8ac9ac5c7d5454612be5a0fa202d126e
TAEGOON-KIM <kkt24682@gmail.com>
2023-07-22 04:12:53 +0900

    metadata 수정

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,23 +3,23 @@ import Head from 'next/head';
 import { ReactNode } from 'react';
 import '../styles/globals.css';
 
-export const metadata = {
-  title: '김경태 ❤️ 김혜진',
-  ogTitle: '김경태 ❤️ 김혜진 청첩장',
-  description: '김경태 ❤️ 김혜진 5월 4일에 결혼합니다.',
-  ogDescription: '5월 4일 1:10PM @ MJ 컨벤션 - 소사역',
-};
+// export const metadata = {
+//   title: '김경태 ❤️ 김혜진',
+//   ogTitle: '김경태 ❤️ 김혜진 청첩장',
+//   description: '김경태 ❤️ 김혜진 5월 4일에 결혼합니다.',
+//   ogDescription: '5월 4일 1:10PM @ MJ 컨벤션 - 소사역',
+// };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
+        <title>김경태 ❤️ 김혜진</title>
+        <meta name="description" content="김경태 ❤️ 김혜진 5월 4일에 결혼합니다."/>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
         <meta property="og:type" content="article"/>
-        <meta property="og:title" content={metadata.ogTitle}/>
-        <meta property="og:description" content={metadata.ogDescription}/>
+        <meta property="og:title" content="김경태 ❤️ 김혜진 청첩장"/>
+        <meta property="og:description" content="5월 4일 1:10PM @ MJ 컨벤션 - 소사역"/>
         <meta property="og:image" content="https://drive.google.com/uc?id=1qFQb88FxV1o9-WOb1E2gJv34vn20_7K2"/>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
--- next.config.js
+++ next.config.js
@@ -3,8 +3,7 @@ const nextConfig = {
     // output: 'export',
     images: {
         unoptimized: true, // 이미지 최적화 기능을 비활성화합니다.
-    },
-    experimental: {reactServerComponents: false}
+    }
 }
 
 module.exports = nextConfig
