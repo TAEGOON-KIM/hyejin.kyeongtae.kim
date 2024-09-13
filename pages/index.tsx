ee810a5177450185f4787c672b3699bb94db4ec5
Kyeongtae Kim <kkt24682@gmail.com>
2024-09-13 16:19:04 +0900

    Redeploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -209,13 +209,13 @@ export default function Home() {
                     <strong>🤵🏻신랑측</strong>
                     <br />
                     (김경태) {accountNumberKT}
-                    <button aria-label="복사" className={styles.copy} onClick={() => handleCopy(accountNumberKT)}>
+                    {/* <button aria-label="복사" className={styles.copy} onClick={() => handleCopy(accountNumberKT)}>
                         <svg width="1.5em" height="1.5em" strokeWidth={1.5} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="currentColor">
                             <path d="M19.4 20H9.6a.6.6 0 01-.6-.6V9.6a.6.6 0 01.6-.6h9.8a.6.6 0 01.6.6v9.8a.6.6 0 01-.6.6z" stroke="currentColor" strokeLinecap='round' strokeLinejoin='round'></path>
                             <path d="M15 9V4.6a.6.6 0 00-.6-.6H4.6a.6.6 0 00-.6.6v9.8a.6.6 0 00.6.6H9" stroke="currentColor" strokeLinecap='round' strokeLinejoin='round'></path>
                         </svg>
-                    </button>
-                    <br />
+                    </button> */}
+                    {/* <br /> */}
                     <a href="http://kko.to/PGiGunjF-u">
                         <img src="/images/kakaopay-logo.png" height="17" />
                     </a>
