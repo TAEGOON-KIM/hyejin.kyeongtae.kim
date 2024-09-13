8ec47ef2fc3c13b2063a4fde2b6444088798a725
Kyeongtae Kim <kkt24682@gmail.com>
2024-09-13 16:28:59 +0900

    Redeploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -256,12 +256,12 @@ export default function Home() {
                     </button>
                     <br />
                     (최종랑) {accountNumberJR}
-                    <button aria-label="복사" className={styles.copy} onClick={() => handleCopy(accountNumberJR)}>
+                    {/* <button aria-label="복사" className={styles.copy} onClick={() => handleCopy(accountNumberJR)}>
                         <svg width="1.5em" height="1.5em" strokeWidth={1.5} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="currentColor">
                             <path d="M19.4 20H9.6a.6.6 0 01-.6-.6V9.6a.6.6 0 01.6-.6h9.8a.6.6 0 01.6.6v9.8a.6.6 0 01-.6.6z" stroke="currentColor" strokeLinecap='round' strokeLinejoin='round'></path>
                             <path d="M15 9V4.6a.6.6 0 00-.6-.6H4.6a.6.6 0 00-.6.6v9.8a.6.6 0 00.6.6H9" stroke="currentColor" strokeLinecap='round' strokeLinejoin='round'></path>
                         </svg>
-                    </button>
+                    </button> */}
                 </p>
             </div>
 
